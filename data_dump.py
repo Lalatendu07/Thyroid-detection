26f4e0fed98358f12fc9859f7006b34ecab7c0c4
Lalatendu07 <lalatendu.4391@gmail.com>
2023-11-16 13:01:25 +0530

    files added

---
+++ data_dump.py
@@ -0,0 +1,25 @@
+import pymongo
+import pandas as pd
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+DATA_FILE_PATH = "/config/workspace/hypothyroid.csv"
+
+DATABASE_NAME = "Thyroid_database"
+
+COLLECTION_NAME = "thyroid_data"
+
+if __name__ == "__main__":
+
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f'Rows and Column : {df.shape}')
+
+    # Convert dataframe to json format
+    df.reset_index(drop=True , inplace=True)
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    # Insert the converted json record to mongo db
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
