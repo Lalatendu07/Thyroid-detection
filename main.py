6fa30285a601251625961d660fcbb40a8aa7ab3d
Lalatendu07 <lalatendu.4391@gmail.com>
2023-12-10 22:57:57 +0530

    filr updated

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from thyroid_detector.utils import get_collection_as_dataframe
+from thyroid_detector.exception import MushroomException
+from thyroid_detector.entity import config_entity
+from thyroid_detector.components.data_ingestion import DataIngestion 
+import os, sys
 
+
+#Data Ingestion
+training_pipeline_config = config_entity.TrainingPipelineConfig()
+data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
--- thyroid_detector/entity/config_entity.py
+++ thyroid_detector/entity/config_entity.py
@@ -1,5 +1,33 @@
+import os,sys
+from datetime import datetime
+from thyroid_detector.exception import ThyroidException
 
-class DataIngestionConfig:...
+FILE_NAME = 'thyroid.csv'
+TRAIN_FILE_NAME = 'train.csv'
+TEST_FILE_NAME = 'test.csv'
+
+class TrainingPipelineConfig:
+
+    def __init__(self):
+        try:
+            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}") 
+        except Exception as e :
+            raise ThyroidException(e , sys)
+
+class DataIngestionConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        try:
+            self.database_name = "Thyroid_database"
+            self.collection_name = "thyroid_data"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+            self.features_store_file_path = os.path.join(self.data_ingestion_dir,'features_store',FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,'dataset',TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir,'dataset',TEST_FILE_NAME)
+            self.test_size = 0.2
+        except Exception as e :
+            raise ThyroidException(e, sys)
+
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
